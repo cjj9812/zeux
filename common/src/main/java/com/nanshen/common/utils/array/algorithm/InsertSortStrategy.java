b660a4127a47d2b9d539c06ae34a135d5b70335c
nanshen <15521031852@163.com>
2019-09-19 20:57:47 +0800

    add：增加插入排序升序策略实现类

---
+++ common/src/main/java/com/nanshen/common/utils/array/algorithm/InsertSortStrategy.java
@@ -0,0 +1,19 @@
+package com.nanshen.common.utils.array.algorithm;
+
+/**
+ * 类描述：插入排序实现类
+ *
+ * @author: scnu_chennanshen
+ * @date: 2019/9/19 10:39
+ */
+public class InsertSortStrategy implements SortStrategy {
+    @Override
+    public Integer[] sortAsce(Integer[] array) {
+        return new Integer[0];
+    }
+
+    @Override
+    public Integer[] sortDesc(Integer[] array) {
+        return new Integer[0];
+    }
+}
--- common/src/main/java/com/nanshen/common/utils/array/algorithm/SortStrategy.java
+++ common/src/main/java/com/nanshen/common/utils/array/algorithm/SortStrategy.java
@@ -5,5 +5,4 @@ public interface SortStrategy {
 
     Integer[] sortDesc(Integer[] array);
 
-
 }
